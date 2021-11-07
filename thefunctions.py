0d36be9c4254b69e59b756e4d085d41a597585ae
StepanS-code <93531033+StepanS-code@users.noreply.github.com>
2021-11-07 14:17:17 +0200

    Update thefunctions.py
    Added functions that get the index of the table, the header of the row with populations as well as the header of the row with country names.

--- thefunctions.py
+++ thefunctions.py
@@ -35,4 +35,65 @@ def GetPopulations(dictionary):
                 except:
                     print(f'The country called {fullName} cannot be found')
                     continue
+# Defining get index 
+def GetTableIndex(url):
+    html = rs.get(url).content
+    df_list = pd.read_html(html)
+    a = -1
+    for table in df_list:
+        a +=1
+        for header in table:
+            for row in table.loc[:,header]:
+                if row == "Ukraine":
+                    b=a
+    return(b)
+# Get name colomn
+def GetPopColomn(url):
+    html = rs.get(url).content
+    df_list = pd.read_html(html)
+    b = "loser"
+    for table in df_list:
+        if b == "sex":
+            break
+        for header in table:
+            print(header)
+            if b == "sex":
+                c = header
+                break
+            for row in table.loc[:,header]:
+                if row == "Ukraine":
+                    b = "sex"
+    return(c)
+
+def GetNameColomn(url):
+        html = rs.get(url).content
+        df_list = pd.read_html(html)
+        d = "loser"
+        for table in df_list:
+            for header in table:
+                for row in table.loc[:,header]:
+                    if row == "Ukraine":
+                        d = header
+                        break
+                    
+        return(d)
+# Defining creeating dictionary for countries:
+def GetDict(url):
+    names = list()
+    populations = list()
+    import pandas as pd
+    import requests as rs
+    html = rs.get(url).content
+    df_list = pd.read_html(html)    
+    df=df_list[GetTableIndex(url)]
+    for name in df.loc[:,GetNameColomn(url)]:
+        if "(" in name:
+            first = name.find("(")
+            name=name[:first-1]
+            names.append(name)
+        else: names.append(name)
+    for population in df.loc[:,GetPopColomn(url)]:
+        populations.append(population)
+    countries = dict(zip(names,populations))
+    return countries
 
